81ee3021ed007996eaa5ec4ad9ff0e068491128b
costabrunamaciel <costabrunamaciel@gmail.com>
2026-08-12 17:12:35 -0300

    Configura relatorio Mochawesome no Cypress e corrige baseUrl ausente

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,20 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  e2e: {
-    baseUrl: "http://localhost:3000",
-    specPattern: "cypress/e2e/**/*.cy.js",
-    supportFile: false,
+  reporter: 'cypress-mochawesome-reporter',
+
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'Livros API - Cypress Test Report',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false
   },
-});
+
+  e2e: {
+    baseUrl: 'http://localhost:3000',
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on)
+    },
+  },
+})
